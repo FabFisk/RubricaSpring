43a972590d69c4bd6fc297ebe2b3acb79165c192
FabFisk <fischettofabrzio@gmail.com>
2016-11-07 16:33:23 +0100

    Added HibernateUtil and MainTest

---
+++ RubricaSpring/src/main/java/it/alfasoft/fabrizio/service/MainTest.java
@@ -0,0 +1,16 @@
+package it.alfasoft.fabrizio.service;
+
+import it.alfasoft.fabrizio.model.Rubrica;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		
+		Gestione g = new Gestione();
+		Rubrica r = new Rubrica();
+		r.setNome("RubricaProva");
+		
+
+	}
+
+}
